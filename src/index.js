412949f16c4ed6101285d0198bfb67bd067596bf
praizoh <oremei.akande@gmail.com>
2022-10-30 01:08:57 +0100

    make nav bar a component of its own

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 const stickyNav = document.getElementById("navbar-sticky");
+const subNav = document.getElementById('sub-nav');
+function show () {
+
+  if (subNav.classList.contains('hidden')){
+    subNav.classList.remove('hidden')
+  }else{
+    subNav.classList.add('hidden')
+  }
+}
 function openMenu(){
   console.log(stickyNav)
   if (stickyNav.classList.contains('hidden')){
